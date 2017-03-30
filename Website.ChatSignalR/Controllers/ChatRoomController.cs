2a15ab513c70386ae40778d0842409539e1cdf47
Vsirius-PC\Vsirius <truongduchuy182@gmail.com>
2017-03-30 15:39:42 +0700

    fix - updatr chtat

---
+++ Website.ChatSignalR/Controllers/ChatRoomController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Website.ChatSignalR.Controllers
+{
+    public class ChatRoomController : Controller
+    {
+        // GET: ChatRoom
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SignalRChat()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Website.ChatSignalR/Hubs/ChatHub.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace Website.ChatSignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        static ConcurrentDictionary<string, string> dic = new ConcurrentDictionary<string, string>();
+
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+
+        public void Send(string name, string message)
+        {
+            Clients.All.broadcastMessage(name, message);
+        }
+
+        public void SendToSpecific(string name, string message, string to)
+        {
+            Clients.Caller.broadcastMessage(name, message);
+            Clients.Client(dic[to]).broadcastMessage(name, message);
+        }
+
+        public void Notify(string name, string id)
+        {
+            if (dic.ContainsKey(name))
+            {
+                Clients.Caller.differentName();
+            }
+            else
+            {
+                dic.TryAdd(name, id);
+                foreach (KeyValuePair<String, String> entry in dic)
+                {
+                    Clients.Caller.online(entry.Key);
+                }
+                Clients.Others.enters(name);
+            }
+        }
+
+        public override Task OnConnected()
+        {
+            // get customerid cookie
+            Cookie s;
+            Context.Request.Cookies.TryGetValue("chat_customerid", out s);
+
+            string customerId = s?.Value ?? null; // Context.RequestCookies["chat_customerid"]?.Value;
+            if(customerId == null)
+            {
+                // add new customerid & save to cookie
+                var httpContext = Context.Request.GetHttpContext();
+                httpContext.Response.SetCookie(new HttpCookie("chat_customerid", "1"));
+            }
+
+            return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            var name = dic.FirstOrDefault(x => x.Value == Context.ConnectionId.ToString());
+            string s;
+            dic.TryRemove(name.Key, out s);
+            return Clients.All.disconnected(name.Key);
+            //base.OnDisconnected(stopCalled: true);
+        }
+    }
+}
